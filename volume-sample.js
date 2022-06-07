493c239f90e007262334e4b2812af6739e32f0a5
Zoe McCloskey <zmccloskey@gmail.com>
2022-06-07 16:02:22 -0700

    first commit

---
+++ volume-sample.js
@@ -0,0 +1,53 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World Again!');
+  <p><input type="button" onclick="VolumeSample.toggle();" value="Play/Pause"</p>
+  <p><input type="range" min="0" max="100" value"100" output="vloumneSample.changeVolumne(this);"==$0></p>
+}).listen(8080);
+
+
+var VolumeSample = {
+};
+
+// Gain node needs to be mutated by volume control.
+VolumeSample.gainNode = null;
+
+VolumeSample.play = function() {
+  if (!context.createGain)
+    context.createGain = context.createGainNode;
+  this.gainNode = context.createGain();
+  var source = context.createBufferSource();
+  source.buffer = BUFFERS.techno;
+
+  // Connect source to a gain node
+  source.connect(this.gainNode);
+  // Connect gain node to destination
+  this.gainNode.connect(context.destination);
+  // Start playback in a loop
+  source.loop = true;
+  if (!source.start)
+    source.start = source.noteOn;
+  source.start(0);
+  this.source = source;
+};
+
+VolumeSample.changeVolume = function(element) {
+  var volume = element.value;
+  var fraction = parseInt(element.value) / parseInt(element.max);
+  // Let's use an x*x curve (x-squared) since simple linear (x) does not
+  // sound as good.
+  this.gainNode.gain.value = fraction * fraction;
+};
+
+VolumeSample.stop = function() {
+  if (!this.source.stop)
+    this.source.stop = source.noteOff;
+  this.source.stop(0);
+};
+
+VolumeSample.toggle = function() {
+  this.playing ? this.stop() : this.play();
+  this.playing = !this.playing;
+};
